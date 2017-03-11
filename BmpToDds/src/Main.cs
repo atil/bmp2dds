f9fac4e7e709c55bc932f85d6c2c5f5d722adaa5
atil <hyperion91@gmail.com>
2017-03-11 14:34:00 +0300

    Initial commit;

---
+++ BmpToDds/src/Main.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BmpToDds
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            const string bmpFileName = "Assets/example.bmp";
+
+            var bmpBytes = File.ReadAllBytes(bmpFileName);
+            Console.WriteLine(bmpBytes.Length);
+            Console.ReadLine();
+        }
+    }
+}
